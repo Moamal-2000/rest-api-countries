3b6c2bff984e70067c4e9c9d6ef20fcba8b95da3
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2023-08-20 16:51:08 +0300

    optimization and arranged javascript code

--- main.js
+++ main.js
@@ -1,36 +1,76 @@
 "use strict";
 
+// Selectors
 const darkMode = document.querySelector("header .dark-light-mode"),
   darkModeIconOff = document.createElement("i"),
   darkModeIconOn = document.createElement("i"),
   filter = document.querySelector(".search-container .filter-region"),
-  filterIcon = document.querySelector('.search-container .filter-region i'),
+  filterIcon = document.querySelector(".search-container .filter-region i"),
   filterList = document.querySelector(".search-container .filter-region .list-of-country"),
   filterLimitation = document.querySelector(".search-container .filter-limitation input"),
   countriesSection = document.querySelector("main .countries"),
-  searchInp = document.querySelector(".search-container .input input");
+  searchInp = document.querySelector(".search-container .input input"),
+  searchInpContainer = document.querySelector(".search-container .input"),
+  searchInputAxeIcon = document.querySelector(".search-container .input .delete");
 
 
+
+
+// Variables
+let numOfCountries = parseInt(filterLimitation.value),
+  showedCountries = 0,
+  darkModeToggle = false,
+  filterToggle = false,
+  regionClicked = false,
+  selectedRegion = "all";
+
+
+
+
+// Initiate code
+// Setup dark mode icons
 darkModeIconOff.className = "fa-solid fa-moon";
 darkModeIconOn.className = "fa-regular fa-moon";
 darkMode.prepend(darkModeIconOff);
 darkMode.prepend(darkModeIconOn);
 darkModeIconOff.style.display = "none";
 
-
-
-
-
-let darkModeToggle = false;
-let darkModeToggleLocal = localStorage.getItem('dark-mode-toggle')
+// Set the dark mode state that saved in localStorage
+let darkModeToggleLocal = localStorage.getItem("dark-mode-toggle");
 if (darkModeToggleLocal !== null) {
-  darkModeToggleLocal === 'false'
-  ? darkModeToggle = true
-  : darkModeToggle = false
-  darkModeSwitcher()
+  darkModeToggleLocal === "false"
+    ? (darkModeToggle = true)
+    : (darkModeToggle = false);
+  darkModeSwitcher();
 }
 
+  // Message not found span
+const messageNotFound = document.createElement("span");
+messageNotFound.classList.add("message-not-found");
+messageNotFound.textContent = "The country you looking for is not found.";
+messageNotFound.style.cssText = `
+  font-weight: bold;
+  font-size: 16px;
+`;
 
+  // Message israel not a country
+const messageNotCountry = document.createElement("span");
+messageNotCountry.classList.add("message-not-country");
+messageNotCountry.innerHTML = `The country you looking for is not found. Do you mean <a href="#">Palestine, State of</a>?`;
+
+
+countriesSection.innerHTML = "";
+
+
+
+
+
+// Functions
+(async function renderCountriesInThePage() {
+  const res = await fetch("data.json");
+  const data = await res.json();
+  for (let i = 0; i < numOfCountries; i++) addCountry(data[i]);
+})();
 
 
 
@@ -46,7 +86,7 @@ function darkModeSwitcher() {
     darkModeIconOff.style.display = "block";
     darkModeToggle = !darkModeToggle;
 
-    localStorage.setItem('dark-mode-toggle', darkModeToggle)
+    localStorage.setItem("dark-mode-toggle", darkModeToggle);
   } else {
     document.documentElement.style.setProperty("--background", "");
     document.documentElement.style.setProperty("--text", "");
@@ -54,278 +94,234 @@ function darkModeSwitcher() {
     darkModeIconOn.style.display = "block";
     darkModeIconOff.style.display = "none";
     darkModeToggle = !darkModeToggle;
-    localStorage.setItem('dark-mode-toggle', darkModeToggle)
+    localStorage.setItem("dark-mode-toggle", darkModeToggle);
   }
 }
 
 
-darkMode.addEventListener("click", darkModeSwitcher);
+
+async function getFilteredCountries() {
+  const isOptionAllCountries = selectedRegion.toLowerCase() === "all";
+  const res = await fetch("data.json");
+  const data = await res.json();
+
+  for (let i = 0; i < data.length; i++) {
+    const isItSelectedRegion =
+      selectedRegion.toLowerCase() === data[i].region.toLowerCase();
+    const isNotEnoughShowingCountries = numOfCountries !== showedCountries;
+
+    if (isOptionAllCountries && isNotEnoughShowingCountries) {
+      addCountry(data[i]);
+      showedCountries++;
+      continue;
+    }
+
+    if (isItSelectedRegion && isNotEnoughShowingCountries) {
+      addCountry(data[i]);
+      showedCountries++;
+    }
+  }
+}
 
 
 
-
-
-
-
-let showCountries = parseInt(filterLimitation.value);
-let showedCountries = 0;
-filterLimitation.addEventListener("input", () => {
-  showCountries = parseInt(filterLimitation.value);
+function handleFilterLimitation() {
+  numOfCountries = parseInt(filterLimitation.value);
   countriesSection.textContent = "";
   searchInp.value = "";
   showedCountries = 0;
-
-  (async function getData() {
-    const res = await fetch('data.json')
-    const data = await res.json()
-    for (let i = 0; i < data.length; i++) {
-      if (
-        selectedRegion.toLowerCase() === "all" &&
-        showCountries !== showedCountries
-      ) {
-        addCountry(data[i]);
-        showedCountries++;
-        continue;
-      }
-
-      if (
-        selectedRegion.toLowerCase() === data[i].region.toLowerCase() &&
-        showCountries !== showedCountries
-      ) {
-        addCountry(data[i]);
-        showedCountries++;
-      }
-    }
-  })();
-});
+  getFilteredCountries();
+}
 
 
 
-
-
-let filterToggle = false;
-let regionClicked = false;
-let selectedRegion = "all";
-filter.addEventListener("click", () => {
+function handleRegionsFilter() {
   if (!filterToggle) {
-    filterList.classList.add('active')
-    filterIcon.classList.add('active')
+    filterList.classList.add("active");
+    filterIcon.classList.add("active");
     filterToggle = !filterToggle;
   } else {
     if (!regionClicked) {
-      filterList.classList.remove('active')
-      filterIcon.classList.remove('active')
+      filterList.classList.remove("active");
+      filterIcon.classList.remove("active");
       filterToggle = !filterToggle;
     }
   }
-});
-
-
-
-
-
-window.addEventListener("click", (e) => {
-  if (
-    !e.target.classList.contains("filter-region") &&
-    e.target.textContent !== "Filter by Region" &&
-    e.target.className !== "fa-solid fa-angle-down"
-  ) {
-    if (filterToggle) {
-      filterList.classList.remove('active')
-      filterIcon.classList.remove('active')
-      filterToggle = !filterToggle;
-    }
-  }
-
-  // Check if one of region clicked
-  Array.from(filterList.children).forEach((li) => {
-    if (e.target.textContent === li.textContent) {
-      selectedRegion = li.textContent;
-      regionClicked = true;
-    }
-  });
-
-  // Check if filter is open & check if one of region is clicked
-  if (!filterToggle && regionClicked) {
-    // Reset settings
-    countriesSection.innerHTML = "";
-    showCountries = parseInt(filterLimitation.value);
-    regionClicked = false;
-
-    (async function getData() {
-      const res = await fetch('data.json')
-      const data = await res.json()
-      for (let i = 0; i < data.length; i++) {
-        let selectedRegion = e.target.textContent.toLowerCase();
-        let region = data[i].region.toLowerCase();
-
-        if (selectedRegion === "all") {
-          addCountry(data[i]);
-          showCountries--;
-          continue;
-        }
-
-        if (selectedRegion === region && showCountries !== 0) {
-          addCountry(data[i]);
-          showCountries--;
-        }
-      }
-    })();
-  }
-});
-
-
-
-
+}
 
 
 
 function addCountry(data) {
   // country container
-  let country = document.createElement("div");
+  const country = document.createElement("div");
   country.classList.add("country");
   countriesSection.append(country);
 
   // flag img
-  let flag = document.createElement("img");
+  const flag = document.createElement("img");
   flag.src = data.flags.png;
   flag.classList.add("flag");
-  flag.setAttribute('alt', 'flag country')
+  flag.setAttribute("alt", "flag country");
   country.append(flag);
 
   // content div
-  let content = document.createElement("div");
+  const content = document.createElement("div");
   content.classList.add("content");
   country.append(content);
 
   // name country
-  let nameCountry = document.createElement("h2");
+  const nameCountry = document.createElement("h2");
   nameCountry.textContent = data.name;
   nameCountry.classList.add("name-country");
   content.append(nameCountry);
 
-  nameCountry.addEventListener('click', () => {
-    localStorage.setItem('name-country', data.name)
-    location.href = 'country-info.html'
-  })
+  nameCountry.addEventListener("click", () => {
+    localStorage.setItem("name-country", data.name);
+    location.href = "country-info.html";
+  });
 
   // population
-  let population = document.createElement("div");
+  const population = document.createElement("div");
   population.innerHTML = `Population: <span>${data.population}</span`;
   population.classList.add("population");
   content.append(population);
 
   // region
-  let region = document.createElement("div");
+  const region = document.createElement("div");
   region.innerHTML = `Region: <span>${data.region}</span`;
   region.classList.add("region");
   content.append(region);
 
   // capital
-  let capital = document.createElement("div");
+  const capital = document.createElement("div");
   capital.innerHTML = `Capital: <span>${data.capital}</span`;
   capital.classList.add("capital");
   content.append(capital);
 }
 
 
-
-
-
-countriesSection.innerHTML = "";
-
-(async function getData() {
-  const res = await fetch('data.json')
-  const data = await res.json()
-  for (let i = 0; i < 20; i++) addCountry(data[i]);
-})();
-
-
-
-
-
-
-// Message not found span
-const messageNotFound = document.createElement("span");
-messageNotFound.classList.add("message-not-found");
-messageNotFound.textContent = "The country you looking for is not found.";
-messageNotFound.style.cssText = `
-  font-weight: bold;
-  font-size: 16px;
-`;
-
-// Message not a country
-const messageNotCountry = document.createElement("span");
-messageNotCountry.classList.add("message-not-country");
-messageNotCountry.innerHTML = `The country you looking for is not found. Do you mean <a href="#">Palestine, State of</a>?`;
-
-
-
-
-messageNotCountry.addEventListener('click', () => {
-  console.log(messageNotCountry.children[0].textContent);
-  localStorage.setItem('name-country', messageNotCountry.children[0].textContent)
-  location.href = 'country-info.html'
-})
-
-
-
-
-
-const searchInpContainer = document.querySelector('.search-container .input')
-const searchInpDeleteBtn = document.querySelector('.search-container .input .delete')
-searchInpContainer.addEventListener('click', () => {
-  searchInpContainer.classList.add('active')
-  searchInpDeleteBtn.classList.add('active')
-  searchInp.focus()
-})
-
-
-
-
-
-
-window.addEventListener('click', (e) => {
-
-  if (
-    !e.target.classList.contains('search') &&
-    !e.target.classList.contains('delete') &&
-    !e.target.classList.contains('input') &&
-    !e.target.classList.contains('search-input')
-    ) {
-    searchInpContainer.classList.remove('active')
-    searchInpDeleteBtn.classList.remove('active')
-  }
-})
-
-
-
-searchInpDeleteBtn.addEventListener('click', () => {
-  searchInp.value = ''
-})
-
-
-
-searchInp.addEventListener("input", () => {
+function handleSearchInput() {
   countriesSection.innerHTML = "";
-  showCountries = parseInt(filterLimitation.value);
+  numOfCountries = parseInt(filterLimitation.value);
 
   (async function getData() {
-    const res = await fetch('data.json')
-    const data = await res.json()
-      // Show specific country
-      for (let i = 0; i < data.length; i++) {
-        let countryName = data[i].name.toLowerCase();
-        let inpText = searchInp.value.toLowerCase();
-        if (countryName.includes(inpText) && showCountries !== 0) {
-          addCountry(data[i]);
-          showCountries--;
-        } else if (inpText === "israel")
-          countriesSection.appendChild(messageNotCountry);
-      }
+    const res = await fetch("data.json");
+    const data = await res.json();
+    // Show specific country
+    for (let i = 0; i < data.length; i++) {
+      let countryName = data[i].name.toLowerCase();
+      let inpText = searchInp.value.toLowerCase();
+      if (countryName.includes(inpText) && numOfCountries !== 0) {
+        addCountry(data[i]);
+        numOfCountries--;
+      } else if (inpText === "israel")
+        countriesSection.appendChild(messageNotCountry);
+    }
 
-      if (countriesSection.children.length === 0)
-        countriesSection.appendChild(messageNotFound);
+    if (countriesSection.children.length === 0)
+      countriesSection.appendChild(messageNotFound);
   })();
+}
+
+
+
+function handleGeneralClick(e) {
+  const target = e.target
+  const notContainsClassFilterRegion = !target.classList.contains("filter-region")
+  const notEqualToText = target.textContent !== "Filter by Region"
+  const notAngleDownIcon = target.className !== "fa-solid fa-angle-down"
+
+
+  // Toggle filter by regions
+  if (notContainsClassFilterRegion && notEqualToText && notAngleDownIcon) {
+    if (filterToggle) {
+      filterList.classList.remove("active");
+      filterIcon.classList.remove("active");
+      filterToggle = !filterToggle;
+    }
+  }
+
+
+  // Check if one of region clicked
+  Array.from(filterList.children).forEach((li) => {
+    if (target.textContent === li.textContent) {
+      selectedRegion = li.textContent;
+      regionClicked = true;
+    }
+  });
+
+
+  // Check if filter is open & check if one of region is clicked
+  if (!filterToggle && regionClicked) {
+    // Reset settings
+    countriesSection.innerHTML = "";
+    numOfCountries = parseInt(filterLimitation.value);
+    regionClicked = false;
+
+    (async function getData() {
+      const res = await fetch("data.json");
+      const data = await res.json();
+      for (let i = 0; i < data.length; i++) {
+        let selectedRegion = target.textContent.toLowerCase();
+        let region = data[i].region.toLowerCase();
+
+        if (selectedRegion === "all") {
+          addCountry(data[i]);
+          numOfCountries--;
+          continue;
+        }
+
+        if (selectedRegion === region && numOfCountries !== 0) {
+          addCountry(data[i]);
+          numOfCountries--;
+        }
+      }
+    })();
+  }
+
+  // Remove active class on search input
+  const isNotSearchClassSearch = !target.classList.contains("search")
+  const isNotAxeIcon = !target.classList.contains("delete")
+  const isNotInputTag = !target.classList.contains("input")
+  const isNotSearchClassSearchInput = !target.classList.contains("search-input")
+  const removeClassActive = 
+    isNotSearchClassSearch &&
+    isNotAxeIcon &&
+    isNotInputTag &&
+    isNotSearchClassSearchInput
+
+  if (removeClassActive) {
+    searchInpContainer.classList.remove("active");
+    searchInputAxeIcon.classList.remove("active");
+  }
+}
+
+
+
+
+
+// Events
+darkMode.addEventListener("click", darkModeSwitcher);
+
+filterLimitation.addEventListener("input", () => handleFilterLimitation());
+
+filter.addEventListener("click", () => handleRegionsFilter());
+
+messageNotCountry.addEventListener("click", () => {
+  localStorage.setItem("name-country", messageNotCountry.children[0].textContent);
+  location.href = "country-info.html";
 });
 
+searchInpContainer.addEventListener("click", () => {
+  searchInpContainer.classList.add("active");
+  searchInputAxeIcon.classList.add("active");
+  searchInp.focus();
+});
+
+searchInp.addEventListener("input", () => handleSearchInput());
+
+searchInputAxeIcon.addEventListener("click", () => {
+  searchInp.value = "";
+});
+
+window.addEventListener("click", (e) => handleGeneralClick(e));
